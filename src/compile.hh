3afdcbd4aafc1b9304167d3c909effcf0259c1fe
Czipperz <czipperz@gmail.com>
2016-03-20 19:03:05 -0500

    Add `\file` documentation to the header

--- src/compile.hh
+++ src/compile.hh
@@ -7,6 +7,8 @@ namespace vick {
 namespace compile {
 
 /*!
+ * \file compile.hh
+ * \brief Defines the `compile_project()` command.
  */
 boost::optional<std::shared_ptr<change> >
 compile_project(contents& unused, boost::optional<int> force_prompt = boost::none);
